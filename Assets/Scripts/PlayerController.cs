8ec91c3d348b75361563287d192f077c940e9203
unknown <ahmed.shawkyegy@gmail.com>
2019-11-12 18:17:46 +0200

    add select wepon UI

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject axe, gun;
+    private bool isHoldingAxe, isHoldingGun;
+
+    private void Start()
+    {
+        isHoldingAxe = false;
+        isHoldingGun = false;
+        axe.SetActive(false);
+        gun.SetActive(false);
+    }
+
+    public void HoldAxe()
+    {
+        gun.SetActive(false);
+        axe.SetActive(true);
+        isHoldingAxe = true;
+    }
+
+    public void HoldGun()
+    {
+        axe.SetActive(false);
+        gun.SetActive(true);
+        isHoldingGun = true;
+    }
+
+
+}
